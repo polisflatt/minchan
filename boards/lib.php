a8d129a1e5f4fcfa7b98172944338dfff3a03df7
Polis Flatt <30891943+polisflatt@users.noreply.github.com>
2019-08-15 13:01:40 -0500

    Clean up code.

--- boards/lib.php
+++ boards/lib.php
@@ -1,20 +1,24 @@
 <?php
 
+    /* Return a boolean whether the board exists or not */
     function board_exists($board_name)
     {
         return is_dir("boards/$board_name/");
     }
 
+    /* Get the current count (ID) of the board */
     function board_get_count($board_name)
     {
         return (int) file_get_contents("boards/$board_name/count");
     }
 
+    /* Increment the current count (ID) of the board */
     function board_increment_count($board_name)
     {
         file_put_contents("boards/$board_name/count", board_get_count($board_name) + 1);
     }
 
+    /* Return the JSON of the board */
     function board_get_json($board_name)
     {
         $json = json_decode(file_get_contents(
@@ -24,14 +28,15 @@
         return $json;
     }
 
-    function board_post_thread($board_name, $id, $title, $contents, $author = "Anonymous")
+    /* Post a thread to the board in question. Title is unused */
+    function board_post_thread($board_name, $id, $contents, $author = "Anonymous")
     {
         mkdir("boards/$board_name/threads/$id");
 
         $json_thread_json = 
         [
             "id" => $id,
-            "title" => $title,
+            "title" => "",
             "contents" => $contents,
             "author" => $author,
             "reply_count" => 0,
@@ -42,10 +47,14 @@
         file_put_contents("boards/$board_name/threads/$id/replies.json", "");
     }
 
+    /* Return an array of all the thread ids */
     function board_get_threads($board)
     {
         return array_diff(scandir("boards/$board/threads/"), array('..', '.'));
     }
+
+    /* Get the JSON of a thread in a board */
+    /* A thread is defined by its ID */
 
     function board_get_thread_json($board, $thread)
     {
@@ -56,11 +65,13 @@
         return $json;
     }
 
+    /* Returns boolean whether a thread on a given board exists */
     function board_thread_exists($board, $thread)
     {
         return is_dir("boards/$board/threads/$thread");
     }
 
+    /* Return a JSON array of other JSON objects containing each reply */
     function board_get_thread_replies($board, $thread)
     {
         $json = json_decode(file_get_contents(
@@ -75,6 +86,7 @@
         return $json;
     }
 
+    /* Increment the thread reply count */
     function board_increment_thread_reply_count($board, $thread)
     {
         $json = board_get_thread_json($board, $thread);
@@ -84,12 +96,14 @@
     }
 
     /* Not to be confused with board_get_thread_replies function() */ 
+    /* Get reply count on a thread */
     function board_get_thread_reply_count($board, $thread)
     {
         $json = board_get_thread_json($board, $thread);
         return (int) $json["reply_count"];
     }
 
+    /* Post a reply on a thread */
     function board_post_thread_reply($board, $thread, $id, $author = "Anonymous", $contents)
     {
         $json = board_get_thread_replies($board, $thread);
@@ -107,6 +121,10 @@
         file_put_contents("boards/$board/threads/$thread/replies.json", json_encode($json));
     }
 
+
+    /* Misleading function name. Originally, it was used to format a reply or a thread so that it would be able to add colour to greentexts. */
+    /* However, it also can format comments so that it can point to other IDs */
+    /* Sadly, it is hardcoded right now. */
 
     function greentext($contents)
     {
@@ -130,34 +148,49 @@
 
 
     /* https://thisinterestsme.com/php-check-string-starts-with-string/ */
+    /* Not my code (I am this lazy to make my own string function), but I refurbished it a bit to make it look nicer. */
+    /* I used to place my curly brackets in K&R format, but I realize now that it is obscenely ugly and disgusting. I write my curly braces normally now. */
+    
     function strStartsWith($haystack, $needle, $caseSensitive = true)
     {
-        //Get the length of the needle.
+        /* Get the length of the needle. */
         $length = strlen($needle);
-        //Get the start of the haystack.
+        
+        /* Get the start of the haystack. */
         $startOfHaystack = substr($haystack, 0, $length);
-        //If we want our check to be case sensitive.
-        if($caseSensitive){
-            //Strict comparison.
-            if($startOfHaystack === $needle){
+        
+        /* If we want our check to be case sensitive. */
+        
+        if($caseSensitive)
+        {
+            /* Strict comparison */
+            if ($startOfHaystack === $needle)
                 return true;
-            }
-        } else{
-            //Case insensitive.
-            //If the needle and the start of the haystack are the same.
-            if(strcasecmp($startOfHaystack, $needle) == 0){
+        } 
+        else
+        {
+            /* Case insensitive. */
+            /* If the needle and the start of the haystack are the same. */
+            
+            if (!strcasecmp($startOfHaystack, $needle))
                 return true;
-            }
         }
-        //No matches. Return FALSE.
+        
+        /* No matches. Return FALSE.*/
         return false;
     }
 
+    /* Extract the tripcode password out of a username */
     function get_tripcode_password($username)
     {
-        $tripcode = explode("#", $username)[1];
+        $tripcode = explode("#", $username)[1]; /* Obtain text after the #, which is the password */
         return $tripcode;
     }
+
+    /* Generate a tripcode on basis of username and password, hashed by SHA256 */
+    /* SHA256 is utter overkill for something as simple as a tripcode, but it's funny since */
+    /* QAnon's (conspiracy scary g-man guy) tripcode got cracked multiple times because most text/image boards use DES for their encryption method */
+    /* Which is actually not that secure by today's standards. */
 
     function generate_tripcode($username, $password)
     {
